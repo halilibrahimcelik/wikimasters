d0c037faa51382a858a53bc1b0c5d0bb8cc83954
halilibrahimcelik <halil.celik139@gmail.com>
2026-02-19 14:52:02 +0000

    feat: add type assertion for articles data in ArticlesPage component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import { ArticlesList } from "@/components/features/wikicards/articles-list";
 import { getArticlesFromDB } from "@/lib/data/articles";
+import { ArticleWikiData } from "@/types/api";
 
 export default async function ArticlesPage() {
-  const articles = await getArticlesFromDB();
+  const articles = (await getArticlesFromDB()) as ArticleWikiData[]; // Type assertion to ArticleWikiData[]
 
   return (
     <div className="container mx-auto py-8">
